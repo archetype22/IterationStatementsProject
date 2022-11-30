215d5a299f72ce26c4482d9f6476aaf9acdc2ea1
nathan stephens <nstephens1234567@gmail.com>
2022-11-30 15:29:27 -0600

    initial commit

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 namespace IterationStatements
 {
@@ -7,38 +8,104 @@ namespace IterationStatements
     {
         static void Main(string[] args)
         {
-                //TODO - Read each comment and complete its instruction
-                // like the example below
+            //Exercise 1
+            #region
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine($"{i}");
+            //}
+            //for (int i = 0; i < 5; i++) 
+            //{
+            //    Console.WriteLine($"{i}");
+            //}
+            //for (int i = 0; i < 5; i++) 
+            //{
+            //    Console.WriteLine($"{i}");
+            //}
+            //for (int i = 0; i < 5; i++) 
+            //{
+            //    Console.WriteLine($"{i}");
+            //}
+            //for (int i = 0; i < 5; i++) 
+            //{
+            //    Console.WriteLine($"{i}");
+            //}
+            //for (int i = 0; i < 5; i++) 
+            //{
+            //    Console.WriteLine($"{i}");
+            //}
+            //for (int i = 0; i < 5; i++) 
+            //{
+            //    Console.WriteLine($"{i}");
+            //}
+            //for (int i = 0; i < 5; i++) 
+            //{
+            //    Console.WriteLine($"{i}");
+            //}
+            //for (int i = 0; i < 5; i++) 
+            //{
+            //    Console.WriteLine($"{i}");
+            //}
+            //for (int i = 0; i < 5; i++) 
+            //{
+            //    Console.WriteLine($"{i}");
+            //}
+            //for (int i = 0; i < 5; i++) 
+            //{
+            //    Console.WriteLine($"{i}");
+            //}
+            //for (int i = 0; i < 5; i++) 
+            //{
+            //    Console.WriteLine($"{i}");
+            //}
+            //for (int i = 0; i < 5; i++) 
+            //{
+            //    Console.WriteLine($"{i}");
+            //}
+            //for (int i = 0; i < 5; i++) 
+            //{
+            //    Console.WriteLine($"{i}");
+            //}
+            //for (int i = 0; i < 5; i++) 
+            //{
+            //    Console.WriteLine($"{i}");
+            //}
+            #endregion
 
-                //Create a List called "numbers" - DONE!
-                var numbers = new List<int>();
-            
+            //Exercise 2
+            //TODO - Read each comment and complete its instruction
+            // like the example below
+
+            //Create a List called "numbers" - DONE!
+            var numbers = new List<int>();
+
             //-----START HERE------------------------------------------
             //Create a variable of type int and name it num
             //initialize the variable with a value of 0
-
+            int num = 0;
 
 
             // Create a do-while loop and use the template below:
-
+            do
             {
                 // Increment num by 1
-
+                num++;
                 // Then add num to the collection - numbers
-                    // Hint: reference num inside of the Add method's parentheses
-                numbers.Add(/* num */);
+                // Hint: reference num inside of the Add method's parentheses
+                numbers.Add(num);
 
-            } // <---- While your variable is less than 100
+            } while (num < 100); // <---- While your variable is less than 100
 
 
 
             // Create a while loop
-            // <--- While num is less than 200
+            while (num < 200)// <--- While num is less than 200
             {
                 // Increment num by 1
+                num++;
                 // Then add num to the collection numbers
-                    //HINT: copy how this was done in the do while loop
-
+                //HINT: copy how this was done in the do while loop
+                numbers.Add(num);
             }
 
 
@@ -47,8 +114,11 @@ namespace IterationStatements
 
 
             // Create a foreach loop using the collection - numbers
-                //In the scope of the foreach loop, print each number in numbers
-             
+            //In the scope of the foreach loop, print each number in numbers
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
 
 
             Console.WriteLine("");
@@ -61,9 +131,9 @@ namespace IterationStatements
                 // AND as long as i is greater than or equal to 0
                 // Decrement i by 1
 
-            //start for loop here
+            for (int i = 199; i <= numbers.Count && i >= 0; i--)//start for loop here
             {
-                // place numbers[i] inside of the Console.WriteLine() method
+                Console.WriteLine(numbers[i]); // place numbers[i] inside of the Console.WriteLine() method
             }
 
             //------------End of exercise
